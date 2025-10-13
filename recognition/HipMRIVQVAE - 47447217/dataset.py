02bddc7235075aac16d5072c6f1b979feaf0b5eb
BillNyeARussianSpy <85931923+BillNyeARussianSpy@users.noreply.github.com>
2025-10-13 10:22:33 +1000

    implemented path listing & image pairing

--- recognition/HipMRIVQVAE - 47447217/dataset.py
+++ recognition/HipMRIVQVAE - 47447217/dataset.py
@@ -14,10 +14,8 @@ def to_channels ( arr : np . ndarray , dtype = np . uint8 ) -> np . ndarray :
     return res
 
 
-# 3D image loader (from Shakes)
-
 # load medical image functions
-def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float32,
+def load_data_2D (imageNames, normImage=False, categorical=False, dtype=np.float32,
                  getAffines=False, early_stop=False):
     '''
     Load medical image data from names, cases list provided into a list for each.
@@ -67,3 +65,66 @@ def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float3
         return images, affines
     else:
         return images
+
+
+# Data ingestion pipeline
+from pathlib import Path
+
+ROOT = Path("keras_slices_data") # root folder
+
+def list_paths(root: Path) -> dict:
+    """
+    Collect nii.gz files for each split
+    args:
+        root::Path
+            parent folder location
+    returns
+        dict
+            dictionary with imgs and segmentation
+    """
+    splits = {
+        "train" : {
+            "imgs" : sorted((root / "kera_slices_train").rglob("*.nii.gz")),
+            "segs" : sorted((root / "keras_slices_seg_train").rglob("*nii.gz")),
+        },
+        "test" : {
+            "imgs" : sorted((root / "kera_slices_test").rglob("*.nii.gz")),
+            "segs" : sorted((root / "keras_slices_seg_test").rglob("*nii.gz")),
+        },
+        "val" : {
+            "imgs" : sorted((root / "kera_slices_val").rglob("*.nii.gz")),
+            "segs" : sorted((root / "keras_slices_seg_val").rglob("*nii.gz")),
+        },
+    }
+    return splits
+
+def align_by_name(imgs, segs) -> list:
+    """
+    Images and segmentations are paired by filename (w/o extensions)
+    args:
+        imgs
+            2D scan image files from HipMRI
+        segs
+            Preprocessed segmentations from HipMRI
+    returns
+        img::list
+            list of images
+        segs:list
+            list of segmentations matched with images
+    """
+    # key for similar ordering of imgs & segs
+    key = lambda p : p.stem.replace(".nii", "") if p.suffic == ".gz" else p.stem
+    # build a map w/ key
+    img_map = {key(p): p for p in imgs}
+    seg_map = {key(p): p for p in segs}
+    # sort
+    common = sorted(set(img_map) & set(seg_map))
+    # debug: in case there are missing imgs/segs
+    missing_imgs = sorted(set(seg_map) - set(img_map))
+    missing_segs = sorted(set(img_map) - set(seg_map))
+    
+    if missing_imgs or missing_segs:
+        raise FileNotFoundError(
+            f"Unpaired files: missing_imgs = {missing_imgs[:5]} missing_segs = {missing_segs[:5]}"
+        )
+    return [img_map[k] for k in common], [seg_map[k] for k in common]
